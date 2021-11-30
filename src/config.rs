54832964a169830cb489e805395d2d8f7dd42408
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2021-11-30 13:43:03 +1100

    added ships size config function

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,19 @@
+// GUI
 pub const EMPTY: &str = "░░░";
 pub const SHIP: &str = "▓▓▓";
 pub const SHOT: &str = " ◌ ";
 pub const DAMAGE: &str = "╳╳╳";
 
-pub const SHIP_1_BLOCK_AMOUNT: usize = 3;
-pub const SHIP_2_BLOCK_AMOUNT: usize = 2;
-pub const SHIP_3_BLOCK_AMOUNT: usize = 1;
+// SHIPS
+pub fn get_entitie_size(kind: &str) -> usize {
+	return match kind {
+		"one_block" => 1,
+		"two_block" => 2,
+		"three_block" => 3,
+		_ => 0,
+	};
+}
+
+pub const SHIP_ONE_BLOCK_AMOUNT: usize = 3;
+pub const SHIP_TWO_BLOCK_AMOUNT: usize = 2;
+pub const SHIP_THREE_BLOCK_AMOUNT: usize = 1;
