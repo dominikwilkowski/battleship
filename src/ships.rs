c20afb4806cc94097a82c241bf87d92bfbef0ba3
Dominik Wilkowski <Hi@Dominik-Wilkowski.com>
2021-12-04 13:47:58 +1100

    returned non-private fields

--- src/ships.rs
+++ src/ships.rs
@@ -49,8 +49,8 @@ impl ShipTracker {
 
 	// we use this function for the tests
 	#[allow(dead_code)]
-	pub fn get_ships(&self) -> &Self {
-		self
+	pub fn get_ships(&self) -> (usize, usize, usize) {
+		(self.one_block, self.two_block, self.three_block)
 	}
 }
 
@@ -58,57 +58,36 @@ impl ShipTracker {
 fn keep_track_of_ships() {
 	let mut ships = ShipTracker::new(3, 2, 1);
 
-	let mut ship_slice = ships.get_ships();
-	assert_eq!(ship_slice.one_block, 3);
-	assert_eq!(ship_slice.two_block, 2);
-	assert_eq!(ship_slice.three_block, 1);
+	assert_eq!(ships.get_ships(), (3, 2, 1));
 	let mut this_ship = ships.get_next_unset_ship().unwrap();
 	assert_eq!(this_ship, Ship::OneBlock);
 	ships.set_ship(&this_ship);
 
-	ship_slice = ships.get_ships();
-	assert_eq!(ship_slice.one_block, 2);
-	assert_eq!(ship_slice.two_block, 2);
-	assert_eq!(ship_slice.three_block, 1);
+	assert_eq!(ships.get_ships(), (2, 2, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
 	assert_eq!(this_ship, Ship::OneBlock);
 	ships.set_ship(&this_ship);
 
-	ship_slice = ships.get_ships();
-	assert_eq!(ship_slice.one_block, 1);
-	assert_eq!(ship_slice.two_block, 2);
-	assert_eq!(ship_slice.three_block, 1);
+	assert_eq!(ships.get_ships(), (1, 2, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
 	assert_eq!(this_ship, Ship::OneBlock);
 	ships.set_ship(&this_ship);
 
-	ship_slice = ships.get_ships();
-	assert_eq!(ship_slice.one_block, 0);
-	assert_eq!(ship_slice.two_block, 2);
-	assert_eq!(ship_slice.three_block, 1);
+	assert_eq!(ships.get_ships(), (0, 2, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
 	assert_eq!(this_ship, Ship::TwoBlock);
 	ships.set_ship(&this_ship);
 
-	ship_slice = ships.get_ships();
-	assert_eq!(ship_slice.one_block, 0);
-	assert_eq!(ship_slice.two_block, 1);
-	assert_eq!(ship_slice.three_block, 1);
+	assert_eq!(ships.get_ships(), (0, 1, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
 	assert_eq!(this_ship, Ship::TwoBlock);
 	ships.set_ship(&this_ship);
 
-	ship_slice = ships.get_ships();
-	assert_eq!(ship_slice.one_block, 0);
-	assert_eq!(ship_slice.two_block, 0);
-	assert_eq!(ship_slice.three_block, 1);
+	assert_eq!(ships.get_ships(), (0, 0, 1));
 	this_ship = ships.get_next_unset_ship().unwrap();
 	assert_eq!(this_ship, Ship::ThreeBlock);
 	ships.set_ship(&this_ship);
 
-	ship_slice = ships.get_ships();
-	assert_eq!(ship_slice.one_block, 0);
-	assert_eq!(ship_slice.two_block, 0);
-	assert_eq!(ship_slice.three_block, 0);
+	assert_eq!(ships.get_ships(), (0, 0, 0));
 	assert!(ships.get_next_unset_ship().is_none())
 }
